5a35891b114e996f55a3a5a9c68af814472156c4
BronzeKennen <sotoskand3@gmail.com>
2024-06-12 14:52:50 +0300

    test

--- src/jobCommander.c
+++ src/jobCommander.c
@@ -1,20 +1,45 @@
 #include <stdlib.h> 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 
 void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
+int serverInit() {
+
+    return 0;
+}
+
 int main(int argc, char** argv) {
     if(argc < 4) {
-        printf("Usage: ./jobCommander [serverName] [portNum] [jobCommanderInputCommand]\n");
+        fprintf(stderr,"Usage: ./jobCommander [serverName] [portNum] [jobCommanderInputCommand]\n");
         exit(1);
     }
+    // int status,portNum;
+    struct hostent *server;
+    struct sockaddr_in serverAddr;
+
+
+    // char* buffer;
+
+    // buffer = malloc(100);
+    server = gethostbyname(argv[1]);
+    if(server == NULL) {
+        fprintf(stderr,"Error no such host\n");
+        exit(1);
+    }
+    bzero((char*) &serverAddr,sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    bcopy((char*) server->h_addr_list,(char*) &serverAddr.sin_addr.s_addr,server->h_length);
+
     
 
     return 0;
--- src/jobExecutorServer.c
+++ src/jobExecutorServer.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -12,6 +13,53 @@ void error(const char *msg) {
 
 
 int main(int argc, char** argv) { 
-    printf("Server Initializing..\n");
+    if(argc < 4) {
+        fprintf(stderr,"Usage: ./jobExecutorServer [portNum] [bufferSize] [threadPoolSize]\n");
+    }
+    int sockFd,newSockFd,portNum,concurrentConnections;
+
+    
+    struct sockaddr_in serverAddr,clientAddr;
+    socklen_t clientLen;
+
+    char *buffer;
+    buffer = malloc(100); // will be replaced with actual dynamic allocation
+
+    sockFd = socket(AF_INET,SOCK_STREAM,0);
+    if(sockFd < 0) {
+        error("Error opening socket.\n");
+    }
+
+    portNum = atoi(argv[1]);
+    concurrentConnections = atoi(argv[1]);
+
+    bzero((char*) &serverAddr,sizeof(serverAddr)); //Cleans up buffer or something
+    serverAddr.sin_family = AF_INET; 
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(portNum);
+
+    if(bind(sockFd,(struct sockaddr*) &serverAddr,sizeof(serverAddr)) < 0) {
+        error("Binding Failed.");
+
+    }
+
+    listen(sockFd , concurrentConnections);
+    clientLen = sizeof(clientAddr);
+
+    newSockFd = accept(sockFd,(struct sockaddr*) &clientAddr, &clientLen);
+    if(newSockFd < 0) {
+        error("Error on accept function\n");
+    }
+    printf("Awaiting Client...\n");
+    while(1) {
+        int n = read(newSockFd, buffer, 100);
+        if(n < 0) {
+            error("Error on read");
+        }
+        printf("Client: %s\n",buffer);
+        bzero(buffer,100);
+    }
+    close(newSockFd);
+    close(sockFd);
     return 0;
 }
